911a220f412deeef69d4040e46bfd9b35ecd3ea1
cleyberfaedo <cleyberfaedocc@hotmail.com>
2022-08-18 20:45:05 -0300

    ADD Creat User

--- backend/src/main/java/com/project/uniamerica/descomplica/backend/user/UserEntity.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/user/UserEntity.java
@@ -1,4 +1,22 @@
 package com.project.uniamerica.descomplica.backend.user;
+
+import lombok.Data;
+import org.springframework.scheduling.support.SimpleTriggerContext;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class UserEntity {
 
+    @Id
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
 }
--- backend/src/main/java/com/project/uniamerica/descomplica/backend/userType/UserTypeEntity.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/userType/UserTypeEntity.java
@@ -1,44 +1,18 @@
-package com.project.uniamerica.descomplica.backend.userType;
-import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
+@Data
 public class UserTypeEntity {
+
     @Id
-    @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public UserTypeEntity() {
-
-    }
-
-    public UserTypeEntity(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "UserTypeEntity{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
